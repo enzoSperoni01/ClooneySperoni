076d6d37f4c212fca95f27be1db12d6071814de7
Enzo Speroni <speronienzo20@gmail.com>
2022-03-08 23:58:21 -0300

    Cambiando parrafo del inicio

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Aqui va a estar mi futuro e-commerce...<br />
+          Para <b>Coderhouse</b>!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
