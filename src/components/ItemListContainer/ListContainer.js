346b977a50bb902606d197014b45be28bb72bfd5
Enzo Speroni <speronienzo20@gmail.com>
2022-03-25 00:25:07 -0300

    Preparando entorno para añadir inventario de Peliculas

--- src/components/ItemListContainer/ListContainer.js
+++ src/components/ItemListContainer/ListContainer.js
@@ -5,6 +5,7 @@ const ListMovies = ({children}) => {
         <div className="container-items">
             <h2>{children}</h2>
 
+            {/* Borrar y añadir ItemList */}
             <div className='items'>
                 <ItemCount stock={5} initial={1}/>
                 <ItemCount stock={10} initial={1}/>
---
+++ src/info/data.js
@@ -0,0 +1,22 @@
+const movies = [
+    {
+        id: 1,
+        title: 'The Batman',
+        price: 50,
+        stock: 4
+    },
+    {
+        id: 2,
+        title: 'Joker',
+        price: 40,
+        stock: 8
+    },
+    {
+        id: 3,
+        title: 'The Dark Knight',
+        price: 75,
+        stock: 11
+    }
+];
+
+export default movies;
