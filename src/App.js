73b42dc8a062598b7b91fb944bc4792c6aa69014
Enzo Speroni <speronienzo20@gmail.com>
2022-03-25 00:07:40 -0300

    Modificando y agregando 'ListContainer'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './scss/App.scss';
 import NavBar from './components/NavBar/NavBar';
-import ItemCount from './components/ItemCount/ItemCount';
+import ListMovies from './components/ItemListContainer/ListContainer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
-      <ItemCount stock={5} initial={1}/>
-      <ItemCount stock={10} initial={1}/>
+      <ListMovies>
+        <p>Peliculas de Drama</p>
+      </ListMovies>
     </div>
   );
 }
---
+++ src/components/ItemListContainer/ListContainer.js
@@ -0,0 +1,16 @@
+import ItemCount from '../ItemCount/ItemCount';
+
+const ListMovies = ({children}) => {
+    return(
+        <div className="container-items">
+            <h2>{children}</h2>
+
+            <div className='items'>
+                <ItemCount stock={5} initial={1}/>
+                <ItemCount stock={10} initial={1}/>
+            </div>
+        </div>
+    )
+}
+
+export default ListMovies;
