7d9c131c2613bc4d0513a1618b1723e72b425c02
Enzo Speroni <speronienzo20@gmail.com>
2022-04-09 18:32:41 -0300

    Agregando un 'Loader' a los ItemDetails

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,19 +1,24 @@
 import React, { useState, useEffect } from 'react';
+import CircularProgress from '@mui/material/CircularProgress';
 import movieList from '../../info/data';
 import ItemDetail from '../ItemDetail/ItemDetail';
 
 const ItemDetailContainer = ({id}) => {
-    const [ film, setFilm ] = useState({});
+    const [ film, setFilm ] = useState([]);
+    const [ loading, setLoading ] = useState(false);
 
     const getItem = () => {
         let promise = new Promise((resolve, reject) => {
-            setTimeout( () => resolve(movieList), 2000)
+            setTimeout( () => resolve(movieList), 1000)
         })
         let result = promise;
         return result;
     };
 
     useEffect(() => {
+        setTimeout(() => {
+            setLoading(true);
+        }, 2000)
         getItem()
             .then( data => {
                 const finded = data.find(element => element.id == id);
@@ -22,10 +27,22 @@ const ItemDetailContainer = ({id}) => {
     }, []);
 
     return(
-        <div className="Detail-Container">
-            <h2>Detalle de la Pelicula:</h2>
-            <ItemDetail item={film}/>
-        </div>
+        <>
+            <div className="Detail-Container">
+                {
+                    loading ? (
+                        <>
+                            <h2>Detalle de la Pelicula:</h2>
+                            <ItemDetail item={film}/>
+                        </>
+                    ) : (
+                        <div className='circular-progress'>
+                            <CircularProgress />
+                        </div>
+                    )
+                }
+            </div>
+        </>
     );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -76,18 +76,8 @@ const NavBar = () => {
                             </Button>
                         </li>
                         )
-                        
                     )
                 })}
-                    {/* <li>
-                        <Link to={'/'} className='link-style'><Button>Inicio</Button></Link>
-                    </li>
-                    <li>
-                        <Button>Nosotros</Button>
-                    </li>
-                    <li>
-                        <Link to={'/contact'} className='link-style'><Button>Contacto</Button></Link>
-                    </li> */}
                 </ul>
             </nav>
 
