352dd3533b19d1b97bf880a11d73e862389d2125
Enzo Speroni <speronienzo20@gmail.com>
2022-04-14 21:30:36 -0300

    Problema: Resolver añadir un producto solo en el Item.js

--- src/components/MovieCard/Item.js
+++ src/components/MovieCard/Item.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import { useCartContext } from '../../Context/CartContext';
 import { Link } from 'react-router-dom';
 import Button from '@mui/material/Button';
 import InfoIcon from '@mui/icons-material/Info';
 
 const MovieItem = ({ item }) => {
     const { id, img, label, price, link } = item;
+    const { cartList, agregarAlCarrito } = useCartContext();
+
+    const addToCart = (e) => {
+        e.stopPropagation();
+        agregarAlCarrito(item);
+    }
 
     return(
         <div key={id} className='movie-card'>
@@ -15,6 +24,7 @@ const MovieItem = ({ item }) => {
             </div>
 
             <div className='btn-sect'>
+                <Button className='btn-cart' onClick={addToCart} variant="contained"><AddShoppingCartIcon className='btn-icon' /></Button>
                 <Link to={`/peliculas/${link}`}>
                     <Button className='btn-cart' variant="outlined"><InfoIcon className='btn-icon' /></Button>
                 </Link>
