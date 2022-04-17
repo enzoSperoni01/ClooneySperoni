285aba2ed1269bae79622c14af31d68f185f558c
Enzo Speroni <speronienzo20@gmail.com>
2022-04-16 23:59:11 -0300

    No imprimen las propiedades en el 'Detalle de Producto'

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,30 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import movieList from '../../info/data';
+// import movieList from '../../info/data';
 import ItemDetail from '../ItemDetail/ItemDetail';
-import LinearIndeterminate from '../LinearLoading/Loading'
+import LinearIndeterminate from '../LinearLoading/Loading';
+import { doc, getDoc } from "firebase/firestore";
+import dataBase from "../../firebase";
+import { useParams, useNavigate } from 'react-router-dom';
 
-const ItemDetailContainer = ({link}) => {
+const ItemDetailContainer = ({ link }) => {
+    const { link } = useParams();
+    // const navigate = useNavigate();
+
     const [ film, setFilm ] = useState([]);
     const [ loading, setLoading ] = useState(false);
 
+    const getMovies = async () => {
+        const docRef = doc(dataBase, "peliculas", link);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+            let product = docSnap.data();
+            product.id = docSnap.id;
+            setFilm(product);
+        } else {
+            console.log("No such document!");
+        }
+    }
+
     const getItem = () => {
         let promise = new Promise((resolve, reject) => {
-            setTimeout( () => resolve(movieList), 2000)
+            setTimeout( () => resolve(getMovies()), 2000)
         })
         let result = promise;
         return result;
     };
 
     useEffect(() => {
+        getMovies();
         setTimeout(() => {
             setLoading(true);
-        }, 3000)
+        }, 3000);
         getItem()
             .then( data => {
                 const finded = data.find(element => element.link == link);
                 setFilm(finded);
             } );
-    }, []);
+    }, [link]);
 
     return(
         <>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,19 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import movieList from '../../info/data';   
 import MovieCard from '../MovieCard/Item.js';
 import CircularProgress from '@mui/material/CircularProgress';
+
+// Firebase
+import dataBase from '../../firebase';
+import { collection, getDocs } from 'firebase/firestore'; 
 
 const ItemList = () => {
     const [ films, setFilms ] = useState([]);
 
-    const getFilms = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve(movieList);
-        }, 3500);
-    });
+    // Firestore
+    const getFilms = async () => { 
+        const filmsCollection = collection(dataBase, 'peliculas');
+        const filmsSnapshot = await getDocs(filmsCollection);
+        const filmList = filmsSnapshot.docs.map((doc) => {
+                let movie = doc.data();
+                movie.id = doc.id;
+
+                return movie;
+            }
+        );
+
+        return filmList;
+    };
 
     useEffect(() => {
-        getFilms.then().then( movies => setFilms(movies));
+        getFilms().then( movies => setFilms(movies));
     }, []);
 
     return(
