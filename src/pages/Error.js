f2bde297f7ee021e77bc7a249d9d6ce913c69e53
Enzo Speroni <speronienzo20@gmail.com>
2022-04-29 01:26:47 -0300

    Añadiendo archivo 'Error.js' para un 'Page Not Found'

---
+++ src/pages/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Alert from '@mui/material/Alert';
+
+const NotFoundPage = () => {
+    return(
+        <>
+            <Alert severity="error">Perdon, no se ha encontrado el sitio que buscas</Alert>
+        </>
+    )
+};
+
+export default NotFoundPage;
